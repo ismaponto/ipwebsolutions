fca3dc21eff780fbee4ed6137fa483cc8e6491e7
ismaponto <isma.pontoriero@gmail.com>
2023-10-11 09:48:30 +0200

    megacambios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React from "react";
+
+import NavigationBar from "./Components/NavigationBar"
+import Jumbotron from './Components/Jumbotron';
+// import HighStrategy from "./Components/HighStrategy"
+import Paraquien from './Components/Paraquien';
+
+import CasosdeExito from "./Components/CasosdeExito"
+import Stats from './Components/Stats';
+// import Contacto from "./Components/Contacto"
+import Footer from "./Components/Footer"
+// import SoyMatias from './Components/soymatias';
+import ListaEmail from './Components/ListaEmail';
+import imgUrl from "./Components/background/leaves.webp"
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (<div className="App font-manrope overflow-hidden bg-center bg-repeat bg-blend-multiply"
+    style={{
+      backgroundImage: `url(${imgUrl})`,
+    }} > 
+{/* <Contacto /> */}
+<NavigationBar /> 
+    <Jumbotron />
+{/* <HighStrategy  /> */}
+<Paraquien  />
+<CasosdeExito />
+<ListaEmail />
+{/* <SoyMatias /> */}
+<Stats />
+
+
+<Footer />
+        </div>
+    );
 }
 
 export default App;
