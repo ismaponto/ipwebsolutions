071dfa31d86de8f04fd31df6f1181db7bd30e76e
ismaponto <isma.pontoriero@gmail.com>
2023-10-13 10:01:21 +0200

    eliminar apikey

--- src/controler/submitToGoogleSheets.js
+++ src/controler/submitToGoogleSheets.js
@@ -1,23 +1,22 @@
-import axios from 'axios';
-// import dotenv from 'dotenv';
+// import axios from 'axios';
+// // import dotenv from 'dotenv';
 
-// dotenv.config({ path: './.env' });
+// // dotenv.config({ path: './.env' });
 
-const submitToGoogleSheets = async(data) => {
-    const spreadsheetId = '1DYXqhgcXxGlMNQ8181G97rrDbG4Ap_vJ2CL2hjEGDO8';
-    const sheetName = 'ipWrbSolution';
-    const apiKey = 'AIzaSyDopIp59kIBLUhVB4M25LHvkMzZNeQQT6Q'
-
+// const submitToGoogleSheets = async(data) => {
+//     const spreadsheetId = '1DYXqhgcXxGlMNQ8181G97rrDbG4Ap_vJ2CL2hjEGDO8';
+//     const sheetName = 'ipWrbSolution';
 
 
-    const response = await axios.post(`https://sheets.googleapis.com/v4/spreadsheets/{spreadsheetId}/values/{range}:append`, {
-        values: [data],
-        headers: {
-            'Authorization': `Bearer ${apiKey}`,
-        },
-    });
 
-    return response.data;
-};
+//     const response = await axios.post(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/{range}:append`, {
+//         values: [data],
+//         headers: {
+//             'Authorization': `Bearer ${apiKey}`,
+//         },
+//     });
 
-export default submitToGoogleSheets;
+//     return response.data;
+// };
+
+// export default submitToGoogleSheets;
