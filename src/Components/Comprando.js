2f59e3891d6f742f7aee2a0f0cfbc73736df7a9e
ismaponto <isma.pontoriero@gmail.com>
2023-11-23 11:47:29 +0100

    umbotron

--- src/Components/Comprando.js
+++ src/Components/Comprando.js
@@ -24,24 +24,26 @@ const Comprando = () => {
       </div>
       <div className="mb-8 mt-8 bg-white bg-opacity-50 hadow w-full rounded-lg flex flex-col justify-center items-center">
         <h3 className="mb-8 text-lg font-normal text-blue-300 p-4 lg:text-2xl sm:px-16 lg:px-48">
-              ¿Tienes un plan? ¿Cómo lograrlo tan rápido?
+              Este es el plan que tenemos:
             </h3>
           <ul className="text-gray-50 flex flex-col justify-center items-center text-xl">
             <li className="mb-4">
-              <p className="text-gray-50">En una videollamada inicial definiremos el alcance del servicio que tu empresa necesita.</p>
+              <p className="text-gray-50">En una primera reunion definiremos el alcance del servicio que tu empresa necesita.</p>
             </li>
             <li className="mb-4">
-              <p className="text-gray-50">En solo 2 semanas, tendrás una maqueta de tu nueva página web lista para brillar.</p>
+              <p className="text-gray-50">En solo 2 semanas, tendrás una maqueta de tu nueva página web</p>
             </li>
             <li className="mb-4">
               <p className="text-gray-50">
-                Al final del mes, tu página estará online. ¡Deja de perder tiempo y comienza a enfocarte en hacer crecer tu negocio!
+                Al final del mes, tu página estará online. ¡Ya no perderas tiempo y podras enfocarte en hacer crecer tu negocio!
               </p>
             </li>
           </ul>
           <p className="mb-8 text-lg font-normal text-blue-300 p-4 lg:text-2xl sm:px-16 lg:px-48">
-              Una landing page con formulario de contacto, y sincronizacion con redes por solo 950 Euros. ¡Pero apúrate! Los primeros 10 clientes lo obtendrán por solo 650 euros.
+              Una landing page con formulario de contacto, y sincronizacion con redes por solo 950 Euros
             </p>
+<p> ¡Pero apúrate! Los primeros 10 clientes lo obtendrán por solo 650 euros./</p>           
+
       </div>
 
       <button
