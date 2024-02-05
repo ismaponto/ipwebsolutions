27eca6c48296671b4e6e7291f3b1a6bc6347b6bc
ismaponto <isma.pontoriero@gmail.com>
2024-02-05 22:15:00 +0100

    mailchimp

--- src/index.js
+++ src/index.js
@@ -27,14 +27,28 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<head>
+			{' '}
+			<script id="mcjs">
+				{' '}
+				! function(c, h, i, m, p){' '}
+				{
+					((m = c.createElement(h)),
+					(p = c.getElementsByTagName(h)[0]),
+					(m.async = 1),
+					(m.src = i),
+					p.parentNode.insertBefore(m, p))
+				}(document, "script",
+				"https://chimpstatic.com/mcjs-connected/js/users/5004c1d1405df506f5e1daff7/c5ac9338132814fda782bb170.js");{' '}
+			</script>{' '}
 			<link rel="preconnect" href="https://fonts.googleapis.com" />
-			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-			{/* <!-- Enlace a la fuente Kalnia desde Google Fonts --> */}{' '}
-			<link href="https://fonts.googleapis.com/css2?family=Mina&display=swap" rel="stylesheet" />{' '}
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />{' '}
+			{/* <!-- Enlace a la fuente Kalnia desde Google Fonts --> */}  {' '}
+			<link href="https://fonts.googleapis.com/css2?family=Mina&display=swap" rel="stylesheet" />  {' '}
 		</head>{' '}
+		{' '}
 		<I18nextProvider i18n={i18n}>
-			<RouterProvider router={router} />{' '}
-		</I18nextProvider>,
+			<RouterProvider router={router} /> {' '}
+		</I18nextProvider>,{' '}
 	</React.StrictMode>
 );
 
