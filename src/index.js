cdb7def5dfa9a2ee22910636fab2445bc717f49e
ismaponto <isma.pontoriero@gmail.com>
2024-02-05 22:20:26 +0100

    cambios mailchmp

--- src/index.js
+++ src/index.js
@@ -28,18 +28,7 @@ root.render(
 	<React.StrictMode>
 		<head>
 			{' '}
-			<script id="mcjs">
-				{' '}
-				! function(c, h, i, m, p){' '}
-				{
-					((m = c.createElement(h)),
-					(p = c.getElementsByTagName(h)[0]),
-					(m.async = 1),
-					(m.src = i),
-					p.parentNode.insertBefore(m, p))
-				}(document, "script",
-				"https://chimpstatic.com/mcjs-connected/js/users/5004c1d1405df506f5e1daff7/c5ac9338132814fda782bb170.js");{' '}
-			</script>{' '}
+			
 			<link rel="preconnect" href="https://fonts.googleapis.com" />
 			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />{' '}
 			{/* <!-- Enlace a la fuente Kalnia desde Google Fonts --> */}  {' '}
