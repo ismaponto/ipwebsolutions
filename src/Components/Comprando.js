b64842b990030413b8408800cd5423a431388b67
ismaponto <isma.pontoriero@gmail.com>
2024-02-12 11:46:27 +0100

    cambio title

--- src/Components/Comprando.js
+++ src/Components/Comprando.js
@@ -26,32 +26,32 @@ const Comprando = () => {
         </ul>
         <p className="mb-8 font-normal p-4 sm:px-16 lg:px-48">{t('pricingText')}</p>
         <p>{t('hurryUpText')}</p>
-      </div>
-      <div className="flex sm:w-3-10 py-2 justify-center items-center">
-        <button
-          className="flex justify-center flex-grow-1 text-xl items-center py-3 px-5 font-medium text-gray-200 rounded-lg border-2 hover:bg-red-400 hover:ring-4 hover:ring-red-400 bg-red-500 bm-5"
-          onClick={redirectToStripe}
-        >
-          {t('buyButton')}
-          <svg
-            className="w-3.5 h-3.5 ml-2"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10"
+        </div>
+        <div className="flex sm:w-3-10 py-2 justify-center items-center">
+          <button
+            className="flex justify-center flex-grow-1 text-xl items-center py-3 px-5 font-medium text-gray-200 rounded-lg border-2 hover:bg-red-400 hover:ring-4 hover:ring-red-400 bg-red-500 bm-5"
+            onClick={redirectToStripe}
           >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-          </svg>
-        </button>
-      </div>
-    </section>
-  );
-};
+            {t('buyButton')}
+            <svg
+              className="w-3.5 h-3.5 ml-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </button>
+        </div>
+      </section>
+    );
+  };
 
-export default Comprando;
+  export default Comprando;
